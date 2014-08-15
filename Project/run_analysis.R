b5328d178c44fb749e815f490676c9918f1096e2
bm10354 <bm10354@gmail.com>
2014-08-15 00:08:37 -0400

    Update run_analysis.R
    - Create tidy data frame
    - Ensure working using knitr package and after Session Clear Workspace

--- Project/run_analysis.R
+++ Project/run_analysis.R
@@ -13,9 +13,23 @@ dfTrainActivities <- read.table("train/y_train.txt")
 dfTestActivities <- read.table("test/y_test.txt")
 dfAllActivities <- rbind(dfTrainActivities, dfTestActivities)
 dfActivityNames <- read.table("activity_labels.txt")
-dfAllActivities$activityName <- factor(dfAllActivities$V1, levels = labels$V1, labels = labels$V2)
-dfAllReq <- cbind(dfAllReq, dfAllActivities$activityName )
-names(dfAllReq)[names(dfAllReq) == "dfAllActivities$activityName"] <- "Activity"
+dfAllActivities$activityName <- factor(dfAllActivities$V1, levels = dfActivityNames$V1, labels = dfActivityNames$V2)
+dfAllReq <- cbind(dfAllActivities$activityName, dfAllReq )
+##names(dfAllReq)[names(dfAllReq) == "dfAllActivities$activityName"] <- "Activity"
+names(dfAllReq)[1] <- "Activity"
 
 #4. Set descriptive variable names
-names(dfAllReq)[1:(ncol(dfAllReq)-1)] = as.character(dfFeatures$V2[iFeatures])
+names(dfAllReq)[2:(ncol(dfAllReq))] = as.character(dfFeatures$V2[iFeatures])
+
+#5. Create tidy data set with the average of each variable for each activity and each subject
+dfSubjectTrain <- read.table(file.path("train", "subject_train.txt"))
+dfSubjectTest <- read.table(file.path("test", "subject_test.txt"))
+dfSubject <- rbind(dfSubjectTrain, dfSubjectTest)
+dfAllReq <- cbind(dfSubject, dfAllReq)
+names(dfAllReq)[1] <- "subjectId"
+
+dfTidy <- aggregate(dfAllReq[,3:81], by = list(dfAllReq$Activity, dfAllReq$subjectId), FUN = mean)
+names(dfTidy)[1:2] <- c("Activity", "subjectId")
+attach(dfTidy)
+dfTidy <- dfTidy[order(Activity),]
+detach(dfTidy)
