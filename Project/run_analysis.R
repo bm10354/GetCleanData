53d5da208abf4c2319e36402f6edf07a8369805d
bm10354 <bm10354@gmail.com>
2014-08-21 21:34:45 -0400

    Update run_analysis.R
    - allow the script to be located not in working directory

--- Project/run_analysis.R
+++ Project/run_analysis.R
@@ -1,35 +1,40 @@
-#1.Merge training and test sets to create one data set
+
+#1.MERGE TRAINING AND TEST SETS TO CREATE ONE DATA SET
 dfTrain <- read.table(file.path(getwd(), "train", "X_train.txt"))
 dfTest <- read.table(file.path(getwd(), "test", "X_test.txt"))
 dfAll <- rbind(dfTrain, dfTest)
 
-#2.Extract only mean and standard deviation for each measurement
-dfFeatures <- read.table(file.path(getwd(),"features.txt"))
+#2.EXTRACT ONLY MEAN AND STANDARD DEVIATION FOR EACH MEASUREMENT
+dfFeatures <- read.table(file.path(getwd(), "features.txt"))
 iFeatures <- grep("mean|std", dfFeatures$V2)
 dfAllReq <- dfAll[,iFeatures]
 
-#3.Use descriptive activity names
-dfTrainActivities <- read.table("train/y_train.txt")
-dfTestActivities <- read.table("test/y_test.txt")
+#3.USE DESCRIPTIVE ACTIVITY NAMES
+dfTrainActivities <- read.table(file.path(getwd(), "train", "y_train.txt"))
+dfTestActivities <- read.table(file.path(getwd(), "test", "y_test.txt"))
 dfAllActivities <- rbind(dfTrainActivities, dfTestActivities)
-dfActivityNames <- read.table("activity_labels.txt")
+dfActivityNames <- read.table(file.path(getwd(), "activity_labels.txt"))
 dfAllActivities$activityName <- factor(dfAllActivities$V1, levels = dfActivityNames$V1, labels = dfActivityNames$V2)
 dfAllReq <- cbind(dfAllActivities$activityName, dfAllReq )
-##names(dfAllReq)[names(dfAllReq) == "dfAllActivities$activityName"] <- "Activity"
-names(dfAllReq)[1] <- "Activity"
+###names(dfAllReq)[names(dfAllReq) == "dfAllActivities$activityName"] <- "Activity"
+names(dfAllReq)[1] <- "Activity" #faster and more reiliable than line above
 
-#4. Set descriptive variable names
+#4.SET DESCRIPTIVE VARIABLE(FEATURE) NAMES
 names(dfAllReq)[2:(ncol(dfAllReq))] = as.character(dfFeatures$V2[iFeatures])
 
-#5. Create tidy data set with the average of each variable for each activity and each subject
-dfSubjectTrain <- read.table(file.path("train", "subject_train.txt"))
-dfSubjectTest <- read.table(file.path("test", "subject_test.txt"))
+#5.CREATE FINAL TIDY DATA SET WITH THE AVERAGE OF EACH VARIABLE FOR EACH ACTIVITY AND EACH SUBJECT
+##5.1. Add subjectId column for further aggregation
+dfSubjectTrain <- read.table(file.path(getwd(), "train", "subject_train.txt"))
+dfSubjectTest <- read.table(file.path(getwd(), "test", "subject_test.txt"))
 dfSubject <- rbind(dfSubjectTrain, dfSubjectTest)
 dfAllReq <- cbind(dfSubject, dfAllReq)
 names(dfAllReq)[1] <- "subjectId"
 
+##5.2.Create data.frame with  aggregation by Activity and then subjectId
 dfTidy <- aggregate(dfAllReq[,3:81], by = list(dfAllReq$Activity, dfAllReq$subjectId), FUN = mean)
 names(dfTidy)[1:2] <- c("Activity", "subjectId")
-attach(dfTidy)
-dfTidy <- dfTidy[order(Activity),]
-detach(dfTidy)
+dfTidy <- dfTidy[order(dfTidy$Activity),]
+
+##5.3.Save final data.frame as space delimited file in workspace subdirectory
+dir.create("tidy", showWarnings = FALSE)
+write.table(dfTidy, file=file.path(getwd(),"tidy","tidy.txt"), sep=" ", row.names=FALSE)
